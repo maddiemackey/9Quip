40d00dd88827718c469c0de3e58ed59eec9aebe0
Porter, Kory <kporter@nine.com.au>
2020-12-03 15:07:06 +1100

    added join game and loading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Host from "./components/host/host";
 import Player from "./components/player/player";
-import JoinGameScreen from "./components/player/screens/JoinGame";
 import Header from "./components/header";
 
 class App extends Component {
@@ -11,12 +10,11 @@ class App extends Component {
     return (
       <div>
         <header>
-          <Header/>
+          <Header />
         </header>
         <main>
           <Switch>
             <Route exact path="/" component={Player} />
-            <Route exact path="/join-game" component={JoinGameScreen} />
             <Route path="/host" component={Host} />
           </Switch>
         </main>
