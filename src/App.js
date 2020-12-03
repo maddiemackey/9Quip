f453b55b6a9d78ea4e07ddccf781d32e01d71630
Porter, Kory <kporter@nine.com.au>
2020-12-03 13:48:55 +1100

    adding shared components folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
+import "./App.css";
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import Host from './components/host/host';
-import Player from './components/player/player';
+import Host from "./components/host/host";
+import Player from "./components/player/player";
+import JoinGameScreen from "./components/player/screens/JoinGame";
 
 class App extends Component {
   render() {
     return (
       <div>
         <header>
-          <p>
-            This is A HEADER
-          </p>
+          <p>This is A HEADER</p>
         </header>
         <main>
           <Switch>
             <Route exact path="/" component={Player} />
+            <Route exact path="/join-game" component={JoinGameScreen} />
             <Route path="/host" component={Host} />
           </Switch>
         </main>
@@ -23,6 +23,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
